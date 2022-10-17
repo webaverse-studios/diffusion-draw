faa3d8bf69740c97a80075e902018983b7efdacf
M̵̞̗̝̼̅̏̎͝Ȯ̴̝̻̊̃̋̀Õ̷̼͋N̸̩̿͜ ̶̜̠̹̼̩͒ <shawmakesmagic@gmail.com>
2022-10-16 18:38:55 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const IMAGE_WIDTH = 1024;
 export const IMAGE_HEIGHT = 1024;
-export const STABLE_DIFFUSION_URL = "http://stable-diffusion.webaverse.com/mod";
+export const STABLE_DIFFUSION_URL = "https://stable-diffusion.webaverse.com/mod";
